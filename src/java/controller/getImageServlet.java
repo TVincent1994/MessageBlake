e5180d9338a5f4e759ffc96944cba32a751f4e1f
TV <tbv3113@yahoo.com>
2022-04-26 23:09:49 -0500

    Update getImageServlet.java

--- src/java/controller/getImageServlet.java
+++ src/java/controller/getImageServlet.java
@@ -74,7 +74,7 @@ public class getImageServlet extends HttpServlet {
         
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://instancemtb.cbvlbcyf5pmu.us-west-2.rds.amazonaws.com:3306/messagetheblake", "messageRoot", "messageTheBRoot");
+            con = DriverManager.getConnection("x,x,x,x");
             Statement st = con.createStatement();
             //Query to get the number of rows in a table
             String numCount = "SELECT count(*) FROM comments WHERE C_itemID="+ imageId;
